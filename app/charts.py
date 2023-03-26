ed062d3afc5302cb658c81d4c688b103d78e2999
Jorozco0112 <javierorozco0112@gmail.com>
2023-03-25 21:01:24 -0500

    realizando mejoras al archivo main.py

--- app/charts.py
+++ app/charts.py
@@ -1,17 +1,19 @@
 import matplotlib.pyplot as plt 
 
 
-def generate_bar_chart(labels, values):
+def generate_bar_chart(name, labels, values):
 
   fig, ax = plt.subplots()
   ax.bar(labels, values)
-  plt.show()
+  plt.savefig(f'./imgs/{name}.png')
+  plt.close
 
-def generate_pie_chart(labels, values):
+def generate_pie_chart(name,labels, values):
   fig, ax = plt.subplots()
   ax.pie(values, labels=labels)
   ax.axis('equal')
-  plt.show()
+  plt.savefig(f'./imgs/{name}pie.png')
+  plt.close
 
 if __name__ == '__main__':
   labels = ['a', 'b' , 'c']
--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import read_csv
 import charts
 
 def run():
-  data = read_csv.read_csv('./app/data.csv')
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
@@ -11,7 +11,8 @@ def run():
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
-    charts.generate_bar_chart(labels, values)
+    charts.generate_bar_chart(country['Country/Territory'], labels, values)
+    charts.generate_pie_chart(country['Country/Territory'], labels, values)
   
 
 
